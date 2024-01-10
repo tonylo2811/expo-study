abdebc34c270577e8b939b27b48ca4bcd348b432
Tony Lo <tony.lo@arch-partnership.com>
2024-01-10 18:27:38 +0800

    feat: common style included

---
+++ styles/common.jsx
@@ -0,0 +1,10 @@
+import { StyleSheet } from "react-native";
+
+const commonStyles = StyleSheet.create({
+    button: {
+        alignItems: 'center',
+        backgroundColor: '#DDDDDD',
+        padding: 10,
+    },
+});
+export default commonStyles;
